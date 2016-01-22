419f4d7413299665425923205912fce14f91e670
Piotr Bogun <piotr.bogun@thirdwavellc.com>
2016-01-22 16:30:02 -0600

    Modifies user profile screen and adds user password account information.

--- assets/js/app/edit/app.users.edit.js
+++ assets/js/app/edit/app.users.edit.js
@@ -1,39 +1,42 @@
 angular.module('app')
 .controller('usersEditCtrl',['localStorageService','$scope','$stateParams','$timeout','API',
 function(localStorageService,$scope,$stateParams,$timeout,API){
-    var usersEdit=this;
-    usersEdit.loading=true;
+    var usersEdit = this;
+    usersEdit.loading = true;
 
     API.doAuth()
-    .then(function(){
-        API.cui.getPerson({personId:$stateParams.id})
-        .then(function(res){
-            usersEdit.loading=false;
-            usersEdit.user=res;
-            $scope.$apply();
-        })
-        .fail(function(err){
-            usersEdit.loading=false;
-            console.log(err);
-        });
+    .then(function(res) {
+        return  API.cui.getPerson({personId:$stateParams.id});
+    })
+    .then(function(res){
+        usersEdit.user=res;
+        return API.cui.getPersonPassword({personId: usersEdit.user.id})
+    })
+    .then(function(res){
+        usersEdit.userPassword = res;
+        $scope.$apply();
+        usersEdit.loading = false;
+    })
+    .fail(function(err){
+        $scope.$apply();
+        usersEdit.loading = false;
     });
 
-
     usersEdit.save=function(){
-        usersEdit.saving=true;
-        usersEdit.fail=false;
-        usersEdit.success=false;
+        usersEdit.saving = true;
+        usersEdit.fail = false;
+        usersEdit.success = false;
         API.cui.updatePerson({personId:$stateParams.id,data:usersEdit.user}).
-        then(function(res){
-            $timeout(function(){
-                usersEdit.saving=false;
-                usersEdit.success=true;
+        then(function(res) {
+            $timeout(function() {
+                usersEdit.saving = false;
+                usersEdit.success = true;
             },300);
         })
-        .fail(function(err){
-            $timeout(function(){
-                usersEdit.saving=false;
-                usersEdit.fail=true;
+        .fail(function(err) {
+            $timeout(function() {
+                usersEdit.saving = false;
+                usersEdit.fail = true;
             },300);
         });
     };
