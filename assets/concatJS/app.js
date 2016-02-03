788d877eee42466588518ce4054416bba4dee50e
RicardoAlmeidaMarques <ricardoalmmarques@gmail.com>
2016-02-03 16:21:58 -0600

    more walkup buildout

--- assets/concatJS/app.js
+++ assets/concatJS/app.js
@@ -1092,17 +1092,21 @@ angular.module('app')
 .controller('usersWalkupCtrl',['localStorageService','$scope','Person','$stateParams', 'API','LocaleService',
 function(localStorageService,$scope,Person,$stateParams,API,LocaleService){
     var usersWalkup=this;
+
+    // Variable initialization
     usersWalkup.userLogin={};
     usersWalkup.applications={};
     usersWalkup.registering=false;
     usersWalkup.registrationError=false;
     usersWalkup.applications.numberOfSelected=0;
-    usersWalkup.user={ addresses:[] };
-    usersWalkup.user.addresses[0]={};
+    usersWalkup.user={ addresses:[] }; // We need to initialize these arrays so ng-model treats them as arrays
+    usersWalkup.user.addresses[0]={ streets:[] }; //  rather than objects
+    usersWalkup.user.phones=[];
 
     function handleError(err){
+        console.log('Error!\n');
         console.log(err);
-    }
+    };
 
     API.doAuth()
     .then(function(){
@@ -1110,18 +1114,14 @@ function(localStorageService,$scope,Person,$stateParams,API,LocaleService){
     })
     .then(function(res){ // get all the security questions
         res.splice(0,1);
-
         // Splits questions to use between both dropdowns
         var numberOfQuestions = res.length,
         numberOfQuestionsFloor = Math.floor(numberOfQuestions/2);
-
         usersWalkup.userLogin.challengeQuestions1 = res.slice(0,numberOfQuestionsFloor);
         usersWalkup.userLogin.challengeQuestions2 = res.slice(numberOfQuestionsFloor);
-
         // Preload question into input
         usersWalkup.userLogin.question1 = usersWalkup.userLogin.challengeQuestions1[0];
         usersWalkup.userLogin.question2 = usersWalkup.userLogin.challengeQuestions2[0];
-
         return API.cui.getOrganizations();
     })
     .then(function(res){
@@ -1129,9 +1129,9 @@ function(localStorageService,$scope,Person,$stateParams,API,LocaleService){
     })
     .fail(handleError);
 
-    var searchOrganizations = function() {
+    var searchOrganizations = function(newOrgNameToSearch) {
         if (usersWalkup.orgSearch) {
-            API.cui.getOrganizations({'qs': [['name', usersWalkup.orgSearch.name]]})
+            API.cui.getOrganizations({'qs': [['name', newOrgNameToSearch]]})
             .then(function(res){
                 usersWalkup.organizationList = res;
                 $scope.$apply();
@@ -1144,11 +1144,11 @@ function(localStorageService,$scope,Person,$stateParams,API,LocaleService){
 
     // Populate Applications List
 
-    $scope.$watch('usersWalkup.organization',function(newOrg){ // If the organization selected changes reset all the apps
-        if(newOrg){
+    $scope.$watch('usersWalkup.organization',function(newOrgSelected){ // If the organization selected changes reset all the apps
+        if(newOrgSelected){
             usersWalkup.applications.numberOfSelected=0; // restart the applications process when a new org
             usersWalkup.applications.processedSelected=undefined; // is selected.
-            API.cui.getPackages({'qs': [['owningOrganization.id', newOrg.id]]})
+            API.cui.getPackages({'qs': [['owningOrganization.id', newOrgSelected.id]]})
             .then(function(res){
                 usersWalkup.applications.list=res;
                 $scope.$apply();
@@ -1158,12 +1158,13 @@ function(localStorageService,$scope,Person,$stateParams,API,LocaleService){
     });
 
     // Update the number of selected apps everytime on of the boxes is checked/unchecked
-    usersWalkup.applications.updateNumberOfSelected=function(a){
-        if(a!==null) usersWalkup.applications.numberOfSelected++;
+    usersWalkup.applications.updateNumberOfSelected=function(checkboxValue){
+        if(checkboxValue!==null) usersWalkup.applications.numberOfSelected++;
         else usersWalkup.applications.numberOfSelected--;
     };
 
     // Process the selected apps when you click next after selecting the apps you need
+    // returns number of apps selected
     usersWalkup.applications.process=function(){
         if(usersWalkup.applications.processedSelected) var oldSelected=usersWalkup.applications.processedSelected;
         usersWalkup.applications.processedSelected=[];
@@ -1172,7 +1173,9 @@ function(localStorageService,$scope,Person,$stateParams,API,LocaleService){
                 usersWalkup.applications.processedSelected.push({
                     id:app.split(',')[0],
                     name:app.split(',')[1],
-                    acceptedTos:((oldSelected && oldSelected[i])? oldSelected[i].acceptedTos : false)
+                    acceptedTos:((oldSelected && oldSelected[i])? oldSelected[i].acceptedTos : false) // this fixes an issue
+                    // where removing an app from the selected list that the user had accepted the terms for
+                    // would carry over that acceptance to the next app on the list
                 });
             }
         });
@@ -1183,8 +1186,6 @@ function(localStorageService,$scope,Person,$stateParams,API,LocaleService){
     usersWalkup.applications.searchApplications=function(){
         API.cui.getPackages({'qs': [['name', usersWalkup.applications.search]]})
         .then(function(res){
-            console.log(typeof usersWalkup.applications.search);
-            console.log(res);
             usersWalkup.applications.list = res;
             $scope.$apply();
         })
@@ -1192,13 +1193,13 @@ function(localStorageService,$scope,Person,$stateParams,API,LocaleService){
     };
 
     // Prepare security question account to be posted to API
-    var buildUserSecurityQuestionAccount=function(){
+    var buildUserSecurityQuestionAccount=function(user){
         return [
             {
                 question:{
                     id:usersWalkup.userLogin.question1.id,
                     type:'question',
-                    realm:res.realm
+                    realm:user.realm
                 },
                 answer:usersWalkup.userLogin.challengeAnswer1,
                 index:1
@@ -1207,7 +1208,7 @@ function(localStorageService,$scope,Person,$stateParams,API,LocaleService){
                 question:{
                     id:usersWalkup.userLogin.question2.id,
                     type:'question',
-                    realm:res.realm
+                    realm:user.realm
                 },
                 answer:usersWalkup.userLogin.challengeAnswer2,
                 index:2
@@ -1215,11 +1216,18 @@ function(localStorageService,$scope,Person,$stateParams,API,LocaleService){
         ];
     };
 
+    // Removes unecessary properties from the user object.
+    var buildUserObject=function(){
+        // if (usersWalkup.user.addresses && usersWalkup.user.addresses[0].streets.length===0) usersWalkup.user.addresses[0].streets[0]=;
+        // if (usersWalkup.user.addresses && usersWalkup.user.addresses.length===0) delete usersWalkup.user.addresses;
+        // if (usersWalkup.user.phones && usersWalkup.user.phones.length===0) delete usersWalkup.user.phones;
+    }
 
     usersWalkup.submit = function(){
         // get the title of the country object selected
         usersWalkup.user.addresses[0].country=usersWalkup.userCountry.title;
         usersWalkup.user.organization={id:usersWalkup.organization.id};
+        buildUserObject();
         usersWalkup.user.timezone='EST5EDT';
         // get the current language being used
         if(LocaleService.getLocaleCode().indexOf('_')>-1) usersWalkup.user.language=LocaleService.getLocaleCode().split('_')[0];
@@ -1231,7 +1239,8 @@ function(localStorageService,$scope,Person,$stateParams,API,LocaleService){
                 personId:res.id,
                 data: {
                     version:1,
-                    questions:buildUserSecurityQuestionAccount()
+                    id:res.id,
+                    questions: buildUserSecurityQuestionAccount(res)
                 }
             });
         })
