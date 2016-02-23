868501d0bf4f11453d65d3f8a79224e92e83f6d6
Piotr Bogun <piotr.bogun@thirdwavellc.com>
2016-02-23 16:40:50 -0600

    Small JS cleanup

--- assets/js/app/profiles/app.organization.profile.js
+++ assets/js/app/profiles/app.organization.profile.js
@@ -1,24 +1,21 @@
 angular.module('app')
 .controller('orgProfileCtrl',['$scope','$stateParams','API',
-function($scope,$stateParams,API){
+function($scope,$stateParams,API) {
 
     var orgProfile = this;
-
     orgProfile.organization = {};
     
-    (function() {
-        API.doAuth()
-        .then(function() {
-            return API.cui.getOrganization({organizationId: $stateParams.id});
-        })
-        .then(function(res) {
-            console.log(res);
-            orgProfile.organization = res;
-            $scope.$digest();
-        })
-        .fail(function(error) {
-            console.log(error);
-        });
-    })();
+    API.doAuth()
+    .then(function() {
+        // Get Organization based on url id parameter
+        return API.cui.getOrganization({organizationId: $stateParams.id});
+    })
+    .then(function(res) {
+        orgProfile.organization = res;
+        $scope.$digest();
+    })
+    .fail(function(error) {
+        console.log(error);
+    });
 
 }]);
