50616f2690f5faa90124f626d6c20d7db28cb57d
Piotr Bogun <piotr.bogun@thirdwavellc.com>
2016-04-08 11:19:01 -0500

    Updates tether directory and admin persona buildout updates

--- assets/app/organization/directory/app.organization.directory.js
+++ assets/app/organization/directory/app.organization.directory.js
@@ -10,10 +10,25 @@ function($scope,$stateParams,API) {
 
     // HELPER FUNCTIONS START ------------------------------------------------------------------------
 
-    var handleError = function(err) {
+    var handleError = function handleError(err) {
         orgDirectory.loading = false;
         $scope.$digest();
         console.log('Error', err);
+    };
+
+    var onLoadFinish = function onLoadFinish(organizationResponse) {
+        orgDirectory.organization = organizationResponse;
+        API.cui.getOrganizations()
+        .then(function(res) {
+            orgDirectory.organizationList = res;
+            return API.cui.getPersons({'qs': [['organization.id', orgDirectory.organization.id]]});
+        })
+        .then(function(res) {
+            orgDirectory.userList = res;
+            orgDirectory.loading = false;
+            $scope.$digest();
+        })
+        .fail(handleError);
     };
 
     // HELPER FUNCTIONS END --------------------------------------------------------------------------
@@ -21,15 +36,13 @@ function($scope,$stateParams,API) {
     // ON LOAD START ---------------------------------------------------------------------------------
 
     if (!organizationId) {
-        // If no id parameter is passed load organization directory of logged in user
+        // If no id parameter is passed load directory of logged in user's organization.
         API.cui.getPerson({personId: API.getUser(), useCuid:true})
         .then(function(person) {
             return API.cui.getOrganization({ organizationId: person.organization.id });
         })
         .then(function(res) {
-            orgDirectory.organization = res;
-            orgDirectory.loading = false;
-            $scope.$digest();
+            onLoadFinish(res);
         })
         .fail(handleError);
     }
@@ -37,13 +50,28 @@ function($scope,$stateParams,API) {
         // Load organization directory of id parameter
         API.cui.getOrganization({ organizationId: organizationId })
         .then(function(res) {
-            orgDirectory.organization = res;
-            orgDirectory.loading = false;
-            $scope.$digest();
+            onLoadFinish(res);
         })
         .fail(handleError);
     }
 
     // ON LOAD END -----------------------------------------------------------------------------------
+
+    // ON CLICK START --------------------------------------------------------------------------------
+
+    orgDirectory.getOrgMembers = function getOrgMembers(organizationId, organizationName) {
+        orgDirectory.loading = true;
+        orgDirectory.organization.id = organizationId;
+        orgDirectory.organization.name = organizationName;
+        API.cui.getPersons({'qs': [['organization.id', orgDirectory.organization.id]]})
+        .then(function(res) {
+            orgDirectory.userList = res;
+            orgDirectory.loading = false;
+            $scope.$digest();
+        })
+        .fail(handleError);
+    };
+
+    // ON CLICK END ----------------------------------------------------------------------------------
 
 }]);
