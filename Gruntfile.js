63ef029274020690104bbe9d2050beff6a67ba26
Piotr Bogun <piotr.bogun@thirdwavellc.com>
2016-04-28 16:10:32 -0500

    Deletes concatJS

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
       },
       scripts:{
         files: ['assets/**/*.js'],
-        tasks: ['concat'],
+        tasks: ['concatModules'],
         options: {
           spawn: false,
         },
