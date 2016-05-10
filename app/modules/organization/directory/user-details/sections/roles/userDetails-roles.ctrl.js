2b46267b71968ccec869d460ca933017ccb4deb1
Piotr Bogun <piotr.bogun@thirdwavellc.com>
2016-05-10 16:21:27 -0500

    Updates organization directory user details

--- app/modules/organization/directory/user-details/sections/roles/userDetails-roles.ctrl.js
+++ app/modules/organization/directory/user-details/sections/roles/userDetails-roles.ctrl.js
@@ -1,11 +1,34 @@
 angular.module('organization')
 .controller('userDetailsRolesCtrl',['API','$stateParams','$q',
 function(API,$stateParams,$q) {
+	'use strict';
 
 	const userDetailsRoles = this,
         userId = $stateParams.userID,
         organizationId = $stateParams.orgID;
 
     let apiPromises = [];
+
+    userDetailsRoles.loading = true;
+
+    // ON LOAD START ---------------------------------------------------------------------------------
+
+    apiPromises.push(
+	    API.cui.getPersonRoles({personId: userId})
+	    .then((res) => {
+	    	userDetailsRoles.assignedRoles = res;
+	    })
+    );
+
+    $q.all(apiPromises)
+    .then((res) => {
+    	userDetailsRoles.loading = false;
+    })
+    .catch((error) => {
+		userDetailsRoles.loading = false;
+		console.log(error);
+    });
+
+    // ON LOAD END -----------------------------------------------------------------------------------
 
 }]);
