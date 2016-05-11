bc93f349c9116d14665da3a77a5177a6762dd332
Piotr Bogun <piotr.bogun@thirdwavellc.com>
2016-05-11 14:46:33 -0500

    Adds password change history endpoint

--- app/modules/common/factories/CustomAPI.js
+++ app/modules/common/factories/CustomAPI.js
@@ -34,7 +34,9 @@ angular.module('common')
 
         {cmd:'getOrganizationGrantableCount',            accepts: 'text/plain',                              call: `/service/v3/applications/persons/${ '{organizationId}' }/grantable/count`},
 
-        {cmd: 'getPersonStatusHistory',                 accepts: 'platform.person.status.history.v1',      call: '/person/v3/persons/statusHistory'},
+        {cmd: 'getPersonStatusHistory',                 accepts: 'platform.person.status.history.v1',       call: '/person/v3/persons/statusHistory' },
+
+        {cmd: 'getPersonPasswordChangeHistory', accepts: 'platform.authn.password.change.history.req.v1', call: `/authentication/v4/passwords/changeHistory` },
     ];
 
     const getCallWrappers = (cuiObject) => {
