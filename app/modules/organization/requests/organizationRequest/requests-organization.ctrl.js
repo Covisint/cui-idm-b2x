060c19ec54aa48cfeda5e8b910e8de4e9c8ea377
Piotr Bogun <piotr.bogun@thirdwavellc.com>
2016-05-20 16:38:02 -0500

    Initial buildout of pending organization request.

--- app/modules/organization/requests/organizationRequest/requests-organization.ctrl.js
+++ app/modules/organization/requests/organizationRequest/requests-organization.ctrl.js
@@ -12,38 +12,88 @@ function(API,$stateParams,$q,$state) {
     orgRequest.loading = true;
 
     // HELPER FUNCTIONS START ------------------------------------------------------------------------
+
+    const getPackageServices = (pendingPackage) => {
+        // Get and append services associated with this package
+        return API.cui.getPackageServices({packageId: pendingPackage.servicePackage.id})
+        .then((res) => {
+            pendingPackage.servicePackage.services = res;
+        });
+    };
+
+    const getPackageClaims = (pendingPackage) => {
+        // Get and append claims associated with this package
+        return API.cui.getPackageClaims({qs: [['packageId', pendingPackage.servicePackage.id]]})
+        .then((res) => {
+            pendingPackage.servicePackage.claims = res;
+        });
+    };
+
+    const getPackageDetails = (pendingPackage) => {
+        // Get and append the package details for this package
+        return API.cui.getPackage({packageId: pendingPackage.servicePackage.id})
+        .then((res) => {
+            pendingPackage.servicePackage.packageDetails = res;
+        });
+    };
+
+
     // HELPER FUNCTIONS END --------------------------------------------------------------------------
 
     // ON LOAD START ---------------------------------------------------------------------------------
 
     apiPromises.push(
-    	// Get Person details based on userID param
     	API.cui.getPerson({personId: userId})
     	.then((res) => {
     		orgRequest.user = res;
+            console.log('orgRequest.user',orgRequest.user);
     	})
     );
 
     apiPromises.push(
-    	API.cui.getPersonRequests({requestId: userId})
+    	API.cui.getPersonRequest({requestId: userId})
     	.then((res) => {
-    		orgRequest.userRequest = res;
+    		orgRequest.userOrgRequest = res;
+            console.log('orgRequest.userOrgRequest',orgRequest.userOrgRequest);
     	})
     );
 
     apiPromises.push(
     	API.cui.getOrganization({organizationId: orgId})
     	.then((res) => {
-    		orgRequest.org = res;
+    		orgRequest.organization = res;
+            console.log('orgRequest.organization',orgRequest.organization);
     	})
     );
 
+    apiPromises.push(
+        // Get user's pending service packages
+        API.cui.getPersonPendingServicePackages({qs: [['requestor.id', userId],['requestor.type', 'person']]})
+        .then((res) => {
+            orgRequest.packages = res;
+            let packagePromises = [];
+
+            res.forEach((pendingPackage) => {
+                // For each package get package claims/services/details
+                packagePromises.push(getPackageServices(pendingPackage), getPackageClaims(pendingPackage), getPackageDetails(pendingPackage));
+            });
+
+            $q.all(packagePromises)
+            .then(() => {
+                console.log('orgRequest.packages', orgRequest.packages);
+                orgRequest.loading = false;
+            })
+            .catch((error) => {
+                console.log(error);
+                orgRequest.loading = false;
+            });
+        })
+    );
+
     $q.all(apiPromises)
-    .then(() => {
-    	orgRequest.loading = false;
-    }, (error) => {
-    	orgRequest.loading = false;
-    	console.log(error);
+    .catch((error) => {
+        orgRequest.loading = false;
+        console.log(error);
     });
 
     // ON LOAD END -----------------------------------------------------------------------------------
