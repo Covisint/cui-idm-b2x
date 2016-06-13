14e6b411f64807e5ab6436bd596ed4ad42093347
RicardoAlmeidaMarques <ricardoalmmarques@gmail.com>
2016-06-13 14:04:39 -0500

    adds new grant states

--- app/modules/organization/organization.module.js
+++ app/modules/organization/organization.module.js
@@ -103,6 +103,18 @@ angular.module('organization',[])
             controller: returnCtrlAs('newGrant'),
             access: loginRequired
         })
+        .state('organization.requests.newGrantSearch', {
+            url: '/search?type&category&name&userID&page&pageSize&sortBy',
+            templateUrl: templateBase + 'requests/newGrant/search/search.html',
+            controller: returnCtrlAs('newGrantSearch'),
+            access: loginRequired
+        })
+        .state('organization.requests.newGrantClaims', {
+            url: '/claims?userID',
+            templateUrl: templateBase + 'requests/newGrant/claims/claims.html',
+            controller: returnCtrlAs('newGrantClaims'),
+            access: loginRequired
+        })
         .state('organization.requests.pendingRequests', {
             url: '/pending-requests?userID&orgID',
             templateUrl: templateBase + 'requests/pendingRequestsReview/requests-pendingRequests.html',
