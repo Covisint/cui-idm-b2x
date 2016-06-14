66a534e13e98bfee58be9f2b7ff3b1758eb6d0c8
RicardoAlmeidaMarques <ricardoalmmarques@gmail.com>
2016-06-14 14:11:12 -0500

    adds some comments to datastorage

--- app/modules/common/factories/DataStorage.factory.js
+++ app/modules/common/factories/DataStorage.factory.js
@@ -11,20 +11,14 @@ angular.module('common')
         localStorageService.set('dataStorage',storage)
     }
 
+    /****************************************
+        FOR OBJECT AND ARRAY TYPE DATA
+    ****************************************/
+
+    // completely overrides a type
     dataStorage.setType = (type, data) => {
         initStorageIfUndefined()
         storage[API.getUser()].type = data
-        saveToLocaStorage()
-    }
-
-    dataStorage.appendToType = (type, data) => {
-        initStorageIfUndefined()
-        if(!storage[API.getUser()][type]) storage[API.getUser()][type] = [data]
-        else if(!_.isArray(storage[API.getUser()][type])){
-            throw new Error('Tried appending to an existing data type that is not an array in dataStorage.')
-            return
-        }
-        else storage[API.getUser()][type].push(data)
         saveToLocaStorage()
     }
 
@@ -39,11 +33,13 @@ angular.module('common')
         saveToLocaStorage()
     }
 
+    // deletes the storage for just the currently logged in user
     dataStorage.deleteUserStorage = () => {
         if(storage[API.getUser()]) delete storage[API.getUser()]
         saveToLocaStorage()
     }
 
+    // deletes the storage for every user in local storage
     dataStorage.clear = () => {
         storage = {}
         saveToLocaStorage()
@@ -51,6 +47,25 @@ angular.module('common')
 
     dataStorage.getUserStorage = () => storage[API.getUser()]
 
+    /****************************************
+        FOR ARRAY TYPE DATA ONLY
+    ****************************************/
+
+    // appends to a certain type
+    // NOTE: Make sure to use data that you can later distinguish between using dataThatMatches
+    // I recommend using replaceDataThatMatches instead unless you know what you're doing
+    dataStorage.appendToType = (type, data) => {
+        initStorageIfUndefined()
+        if(!storage[API.getUser()][type]) storage[API.getUser()][type] = [data]
+        else if(!_.isArray(storage[API.getUser()][type])){
+            throw new Error('Tried appending to an existing data type that is not an array in dataStorage.')
+            return
+        }
+        else storage[API.getUser()][type].push(data)
+        saveToLocaStorage()
+    }
+
+    // returns ALL data that matches against a given comparison
     dataStorage.getDataThatMatches = (type, comparison) => {
         if(!storage[API.getUser()] || !storage[API.getUser()][type]) return
         else if(!_.isArray(storage[API.getUser()][type])){
@@ -58,12 +73,12 @@ angular.module('common')
             return
         }
         else {
-            const index = _.findIndex(storage[API.getUser()][type], comparison)
-            if(index >= 0) return storage[API.getUser()][type][index]
-            else return null
+            return storage[API.getUser()][type].filter(x => _.matches(x,comparison))
         }
     }
 
+    // replaces all data that matches against a certain comparison and appends
+    // new data to that type's array
     DataStorage.replaceDataThatMatches = (type, comparison, newData) => {
         dataStorage.deleteDataThatMatches(type, comparison)
         dataStorage.appendToType(type, newData)
