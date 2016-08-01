d040718ae39e9f5acc507d502967be29925cef38
Piotr Bogun <piotr.bogun@thirdwavellc.com>
2016-08-01 11:13:42 -0500

    Adds error handling/notifications to organization profile

--- app/modules/organization/profile/organization-profile.ctrl.js
+++ app/modules/organization/profile/organization-profile.ctrl.js
@@ -1,30 +1,34 @@
 angular.module('organization')
-.controller('orgProfileCtrl', function(API,Loader,User,$scope,$state) {
+.controller('orgProfileCtrl', function(API,APIError,Loader,User,$scope,$state,$timeout) {
 
-    const orgProfile = this;
-    const loaderName = 'orgProfile.';
+    const orgProfile = this
+    const eventName = 'orgProfile.init'
 
-    orgProfile.organization = User.user.organization;
+    orgProfile.organization = User.user.organization
+    orgProfile.organization ? APIError.offFor(eventName) : APIError.onFor(eventName)
 
     /* -------------------------------------------- ON LOAD START --------------------------------------------- */
 
-    Loader.onFor(loaderName + 'loading');
+    if (orgProfile.organization) {
+        Loader.onFor(eventName)
 
-    API.cui.getPersonsAdmins({qs: [['organization.id', orgProfile.organization.id], ['securityadmin', true]]})
-    .then(function(res) {
-        orgProfile.securityAdmins = res;
-        Loader.offFor(loaderName + 'loading');
-        $scope.$digest();
-    })
-    .fail((error) => {
-        console.error('Error', error);
-        if (!orgProfile.organization) {
-            $state.go('misc.loadError');
-        }
-        Loader.offFor(loaderName + 'loading');
-        $scope.$digest();
-    });
+        API.cui.getPersonsAdmins({qs: [['organization.id', orgProfile.organization.id], ['securityadmin', true]]})
+        .done(res => {
+            orgProfile.securityAdmins = res
+        })
+        .fail(err => {
+            console.error('Error getting organization admin information', err)
+            APIError.onFor(eventName)
+            $timeout(() => {
+                APIError.offFor(eventName)
+            }, 5000)
+        })
+        .always(() => {
+            Loader.offFor(eventName)
+            $scope.$digest()
+        })
+    }
 
     /* --------------------------------------------- ON LOAD END ---------------------------------------------- */
 
-});
+})
