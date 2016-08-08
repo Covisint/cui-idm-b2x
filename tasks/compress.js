7a84de0f566ba1daa6f7c259d3db57835585fc89
Orin Fink <orin.fink@thirdwavellc.com>
2016-08-08 17:51:40 -0500

    Resolve incorrect nesting after merge.

--- tasks/compress.js
+++ tasks/compress.js
@@ -1,19 +1,17 @@
 module.exports = function(grunt,config){
     return {
-        build: {
-          options: {
-            archive: './app-' + config.target + '-' + grunt.template.today('yyyymmddhhmmss') + '.zip'
-          },
-          files: [
-            {
-                expand:true,
-                cwd: 'build/',
-                src: ['**','!.DS_Store'],
-                dest: './'
-            }
-          ]
-        }
-      ]
+      build: {
+        options: {
+          archive: './app-' + config.target + '-' + grunt.template.today('yyyymmddhhmmss') + '.zip'
+        },
+        files: [
+          {
+              expand:true,
+              cwd: 'build/',
+              src: ['**','!.DS_Store'],
+              dest: './'
+          }
+        ]
+      }
     }
   }
-}
