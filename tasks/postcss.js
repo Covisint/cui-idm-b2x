9c1af80b536a00ecd16380bc9f555e1deabce1d8
Orin Fink <orin.fink@thirdwavellc.com>
2016-08-29 16:23:14 -0500

    whitespace formatting

--- tasks/postcss.js
+++ tasks/postcss.js
@@ -1,15 +1,15 @@
 module.exports = function(grunt,config){
-    return {
-        options: {
-          map: true, // inline sourcemaps
-          processors: [
-            require('autoprefixer')({browsers: 'last 3 versions'}), // add vendor prefixes
-          ]
-        },
-        dist: {
-          files: {
-            '<%= config.concatCssDir %>': '<%= config.concatCssDir %>'
-          }
-        }
-    };
+  return {
+    options: {
+      map: true, // inline sourcemaps
+      processors: [
+        require('autoprefixer')({browsers: 'last 3 versions'}), // add vendor prefixes
+      ]
+    },
+    dist: {
+      files: {
+        '<%= config.concatCssDir %>': '<%= config.concatCssDir %>'
+      }
+    }
+  };
 };
