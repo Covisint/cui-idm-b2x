384ebbbe14478c0e99728cd9ff8c414a1e70c5a2
Orin Fink <orin.fink@thirdwavellc.com>
2016-08-29 16:22:48 -0500

    Adds feature to actually _deploy_ the artifact file to the UI Proxy Server given that user has either passed in the clientId and clientSecret via command line parameters _or_ the user has a profile set up in their .cui directory of their "HOME".

--- tasks/compress.js
+++ tasks/compress.js
@@ -2,7 +2,7 @@ module.exports = function(grunt,config){
     return {
       build: {
         options: {
-            archive: './build-artifacts/'+ config.name + '-'+ config.version + '-' + config.env + '-' + grunt.template.today('yyyymmddhhMMss') + '.zip'
+            archive: config.artifactDir + '/' + config.buildArtifact
         },
         files: [
           {
