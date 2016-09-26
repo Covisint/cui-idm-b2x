85f87ac12a84dbd9518e6f948a58dffc58075a59
Orin Fink <orin.fink@thirdwavellc.com>
2016-09-26 17:10:59 -0500

    OK, semicolons were ugly.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,21 @@
 module.exports = function(grunt) {
-  require('load-grunt-tasks')(grunt);
-  const path = require('path');
-  const YAML = require('yamljs');
+    require('load-grunt-tasks')(grunt)
+    const path = require('path')
+    const YAML = require('yamljs')
 
-  // Load the package.json file to have its variables available
-  var pkgJson = require('./package.json');
-  const appName = pkgJson.name.split('/').pop();
-  const dtStamp = grunt.template.today('yyyymmddhhMMss');
+    // Load the package.json file to have its variables available
+    var pkgJson = require('./package.json')
+    const appName = pkgJson.name.split('/').pop()
+    const dtStamp = grunt.template.today('yyyymmddhhMMss')
 
     // look for a target specified in the command line, otherwise assume 'local'
-    const target = (typeof grunt.option('target') === 'undefined') ? 'local' : grunt.option('target');
-    const buildArtifact = appName + '-' + pkgJson.version + '-' + target + '-' + dtStamp + '.zip';
+    const target = (typeof grunt.option('target') === 'undefined') ? 'local' : grunt.option('target')
+    const buildArtifact = appName + '-' + pkgJson.version + '-' + target + '-' + dtStamp + '.zip'
 
     // load the secrets profile file from your home directory
-    const envPath = path.join(process.env['HOME'],'.cui/profiles',appName,target);
+    const envPath = path.join(process.env['HOME'],'.cui/profiles',appName,target)
     var clientId, clientSecret, originUri, uiHost, serviceUrl, solutionInstancesUrl
+
     //if the envPath does not exist, create it
     if (!grunt.file.exists(envPath)) {
 
@@ -27,21 +28,21 @@ module.exports = function(grunt) {
         solutionInstancesUrl
       };
 
-      grunt.log.writeln('Creating a profile for you to store secrets.');
+      grunt.log.writeln('Creating a profile for you to store secrets.')
       grunt.file.write(envPath, YAML.stringify(emptyEnv));
-      grunt.log.writeln('An empty profile has been created for you at: ' + envPath);
+      grunt.log.writeln('An empty profile has been created for you at: ' + envPath)
 
     }
 
     // now that we know it exists, we can read it, but trap it just in case
     var env = grunt.file.readYAML(envPath)
 
-    clientId = (typeof grunt.option('clientId') === 'undefined') ? env.clientId : grunt.option('clientId');
-    clientSecret = (typeof grunt.option('clientSecret') === 'undefined') ? env.clientSecret : grunt.option('clientSecret');
-    originUri = (typeof grunt.option('originUri') === 'undefined') ? env.originUri : grunt.option('originUri');
-    uiHost = (typeof grunt.option('uiHost') === 'undefined') ? env.uiHost : grunt.option('uiHost');
-    serviceUrl = (typeof grunt.option('serviceUrl') === 'undefined') ? env.serviceUrl : grunt.option('serviceUrl');
-    solutionInstancesUrl = (typeof grunt.option('solutionInstancesUrl') === 'undefined') ? env.solutionInstancesUrl : grunt.option('solutionInstancesUrl');
+    clientId = (typeof grunt.option('clientId') === 'undefined') ? env.clientId : grunt.option('clientId')
+    clientSecret = (typeof grunt.option('clientSecret') === 'undefined') ? env.clientSecret : grunt.option('clientSecret')
+    originUri = (typeof grunt.option('originUri') === 'undefined') ? env.originUri : grunt.option('originUri')
+    uiHost = (typeof grunt.option('uiHost') === 'undefined') ? env.uiHost : grunt.option('uiHost')
+    serviceUrl = (typeof grunt.option('serviceUrl') === 'undefined') ? env.serviceUrl : grunt.option('serviceUrl')
+    solutionInstancesUrl = (typeof grunt.option('solutionInstancesUrl') === 'undefined') ? env.solutionInstancesUrl : grunt.option('solutionInstancesUrl')
 
     var config = {
       buildDir : './build',
@@ -62,15 +63,16 @@ module.exports = function(grunt) {
       buildArtifact: buildArtifact,
       serviceUrl: serviceUrl,
       solutionInstancesUrl: solutionInstancesUrl
-    };
+    }
 
     // now build the appConfig-env.json
     var appConfigEnv = {
       originUri: config.originUri,
       serviceUrl: config.serviceUrl,
       solutionInstancesUrl: config.solutionInstancesUrl
-    };
-    grunt.file.write('appConfig-env.json', JSON.stringify(appConfigEnv, null, 4));
+    }
+
+    grunt.file.write('appConfig-env.json', JSON.stringify(appConfigEnv, null, 4))
 
     // now build the appConfig-build.json
     var appConfigBuild = {
@@ -79,58 +81,58 @@ module.exports = function(grunt) {
       target: config.target,
       buildDate: grunt.template.today('yyyymmddhhMMss'),
       buildArtifact: config.buildArtifact
-    };
+    }
 
-    grunt.file.write('appConfig-build.json', JSON.stringify(appConfigBuild, null, 4));
+    grunt.file.write('appConfig-build.json', JSON.stringify(appConfigBuild, null, 4))
 
-  var tasks = ['watch','sass','browserSync','postcss','clean','compress','copy','filerev','useminPrepare',
-  'useminPreparesdk','usemin','useminsdk','uglify','ngtemplates','processhtml','babel','ngAnnotate',
-  'http_upload'];
+    var tasks = ['watch','sass','browserSync','postcss','clean','compress','copy','filerev','useminPrepare',
+    'useminPreparesdk','usemin','useminsdk','uglify','ngtemplates','processhtml','babel','ngAnnotate',
+    'http_upload']
 
-  var opts = {
-    config:config
-  };
+    var opts = {
+      config:config
+    }
 
-  tasks.forEach(function(task) {
-    opts[task] = require('./tasks/' + task + '.js')(grunt, config);
-  });
+    tasks.forEach(function(task) {
+      opts[task] = require('./tasks/' + task + '.js')(grunt, config);
+    })
 
-  grunt.initConfig(opts);
+    grunt.initConfig(opts);
 
-  // Workaround for multiple useminPrepare tasks
-  grunt.registerTask('useminPreparesdk', function() {
-    var useminPreparesdkConfig = grunt.config('useminPreparesdk');
-    grunt.config.set('useminPrepare', useminPreparesdkConfig);
-    grunt.task.run('useminPrepare');
-  });
+    // Workaround for multiple useminPrepare tasks
+    grunt.registerTask('useminPreparesdk', function() {
+      var useminPreparesdkConfig = grunt.config('useminPreparesdk');
+      grunt.config.set('useminPrepare', useminPreparesdkConfig);
+      grunt.task.run('useminPrepare');
+    })
 
-  // Workaround for multiple usemin tasks
-  grunt.registerTask('useminsdk', function() {
-    var useminsdkConfig = grunt.config('useminsdk');
-    grunt.config.set('usemin', useminsdkConfig);
-    grunt.task.run('usemin');
-  });
+    // Workaround for multiple usemin tasks
+    grunt.registerTask('useminsdk', function() {
+      var useminsdkConfig = grunt.config('useminsdk');
+      grunt.config.set('usemin', useminsdkConfig);
+      grunt.task.run('usemin');
+    })
 
-  grunt.registerTask('concatModules', 'Task to concat all project modules.',
-    require('./tasks/concatModules.js')(grunt,config));
+    grunt.registerTask('concatModules', 'Task to concat all project modules.',
+      require('./tasks/concatModules.js')(grunt,config))
 
-  // Tasks ------------------------------------------------------------
-  grunt.registerTask('default', ['concatModules','babel','ngAnnotate','sass','postcss','browserSync:dev','watch']);
+    // Tasks ------------------------------------------------------------
+    grunt.registerTask('default', ['concatModules','babel','ngAnnotate','sass','postcss','browserSync:dev','watch'])
 
-  grunt.registerTask('build', ['sass','postcss','processhtml','ngtemplates:build','clean:build','copy:build',
-                                'concatModules','babel','ngAnnotate','useminPrepare','concat:generated',
-                                'cssmin:generated','uglify:generated','filerev:build','usemin','clean:temp']);
+    grunt.registerTask('build', ['sass','postcss','processhtml','ngtemplates:build','clean:build','copy:build',
+                                  'concatModules','babel','ngAnnotate','useminPrepare','concat:generated',
+                                  'cssmin:generated','uglify:generated','filerev:build','usemin','clean:temp'])
 
-  grunt.registerTask('buildsdk', ['sass','postcss','ngtemplates:buildsdk','clean:buildsdk','copy:buildsdk',
-                                  'concatModules','babel','useminPreparesdk','concat:generated','cssmin:generated',
-                                  'uglify:generated','filerev:buildsdk','useminsdk','clean:temp']);
+    grunt.registerTask('buildsdk', ['sass','postcss','ngtemplates:buildsdk','clean:buildsdk','copy:buildsdk',
+                                    'concatModules','babel','useminPreparesdk','concat:generated','cssmin:generated',
+                                    'uglify:generated','filerev:buildsdk','useminsdk','clean:temp'])
 
-  grunt.registerTask('demo', ['browserSync:demo']);
+    grunt.registerTask('demo', ['browserSync:demo'])
 
-  grunt.registerTask('demosdk', ['browserSync:demosdk']);
+    grunt.registerTask('demosdk', ['browserSync:demosdk'])
 
-  grunt.registerTask('package', ['build','compress']);
+    grunt.registerTask('package', ['build','compress'])
 
-  grunt.registerTask('deploy', ['build','compress','http_upload:build']);
+    grunt.registerTask('deploy', ['build','compress','http_upload:build'])
 
-};
+}
