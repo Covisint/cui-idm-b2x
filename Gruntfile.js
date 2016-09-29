9cbd7bfa01ca7454c3a72bbbbd19e87028de3855
Piotr Bogun <piotr.bogun@thirdwavellc.com>
2016-09-29 10:34:02 -0500

    Fixes gruntfile indentations

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,90 +1,88 @@
 module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
+  
   const path = require('path');
   const YAML = require('yamljs');
-
   // Load the package.json file to have its variables available
   var pkgJson = require('./package.json');
   const appName = pkgJson.name.split('/').pop();
   const dtStamp = grunt.template.today('yyyymmddhhMMss');
 
-    // look for a target specified in the command line, otherwise assume 'local'
-    const target = (typeof grunt.option('target') === 'undefined') ? 'local' : grunt.option('target');
-    const buildArtifact = appName + '-' + pkgJson.version + '-' + target + '-' + dtStamp + '.zip';
+  // look for a target specified in the command line, otherwise assume 'local'
+  const target = (typeof grunt.option('target') === 'undefined') ? 'local' : grunt.option('target');
+  const buildArtifact = appName + '-' + pkgJson.version + '-' + target + '-' + dtStamp + '.zip';
 
-    // load the secrets profile file from your home directory
-    // const envPath = path.join(process.env['HOME'],'.cui/profiles',appName,target);
-  var envHome = process.env.HOME || process.env.HOMEPATH ||
-      process.env.USERPROFILE
-  const envPath = path.join(envHome,'.cui/profiles',appName,target)
+  // load the secrets profile file from your home directory
+  var envHome = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE
+  const envPath = path.join(envHome, '.cui/profiles', appName, target)
 
-    //if the envPath does not exist, create it
-    if (!grunt.file.exists(envPath)) {
+  //if the envPath does not exist, create it
+  if (!grunt.file.exists(envPath)) {
 
-      var emptyEnv = {
-        clientId,
-        clientSecret,
-        originUri,
-        uiHost,
-        serviceUrl,
-        solutionInstancesUrl
-      };
-
-      grunt.log.writeln('Creating a profile for you to store secrets.');
-      grunt.file.write(envPath, YAML.stringify(emptyEnv));
-      grunt.log.writeln('An empty profile has been created for you at: ' + envPath);
-
-    }
-
-    // now that we know it exists, we can read it, but trap it just in case
-    var env = grunt.file.readYAML(envPath);
-
-    var clientId = (typeof grunt.option('clientId') === 'undefined') ? env.clientId : grunt.option('clientId');
-    var clientSecret = (typeof grunt.option('clientSecret') === 'undefined') ? env.clientSecret : grunt.option('clientSecret');
-    var originUri = (typeof grunt.option('originUri') === 'undefined') ? env.originUri : grunt.option('originUri');
-    var uiHost = (typeof grunt.option('uiHost') === 'undefined') ? env.uiHost : grunt.option('uiHost');
-    var serviceUrl = (typeof grunt.option('serviceUrl') === 'undefined') ? env.serviceUrl : grunt.option('serviceUrl');
-    var solutionInstancesUrl = (typeof grunt.option('solutionInstancesUrl') === 'undefined') ? env.solutionInstancesUrl : grunt.option('solutionInstancesUrl');
-
-    var config = {
-      buildDir : './build',
-      buildSdkDir : './build-sdk',
-      artifactDir : './build-artifacts',
-      concatAppDir: './assets/concat/js/app.js',
-      concatCssDir: './assets/concat/css/main.css',
-      modules: './app/modules',
-      scss: './app/scss',
-      target: target,
-      version: pkgJson.version,
-      name: appName,
-      userHomeDir: process.env['HOME'],
-      clientId: clientId,
-      clientSecret: clientSecret,
-      originUri: originUri,
-      uiHost: uiHost,
-      buildArtifact: buildArtifact,
-      serviceUrl: serviceUrl,
-      solutionInstancesUrl: solutionInstancesUrl
+    var emptyEnv = {
+      clientId,
+      clientSecret,
+      originUri,
+      uiHost,
+      serviceUrl,
+      solutionInstancesUrl
     };
 
-    // now build the appConfig-env.json
-    var appConfigEnv = {
-      originUri: config.originUri,
-      serviceUrl: config.serviceUrl,
-      solutionInstancesUrl: config.solutionInstancesUrl
-    };
-    grunt.file.write('appConfig-env.json', JSON.stringify(appConfigEnv, null, 4));
+    grunt.log.writeln('Creating a profile for you to store secrets.');
+    grunt.file.write(envPath, YAML.stringify(emptyEnv));
+    grunt.log.writeln('An empty profile has been created for you at: ' + envPath);
+  }
 
-    // now build the appConfig-build.json
-    var appConfigBuild = {
-      app: appName,
-      version: config.version,
-      target: config.target,
-      buildDate: grunt.template.today('yyyymmddhhMMss'),
-      buildArtifact: config.buildArtifact
-    };
+  // now that we know it exists, we can read it, but trap it just in case
+  var env = grunt.file.readYAML(envPath);
 
-    grunt.file.write('appConfig-build.json', JSON.stringify(appConfigBuild, null, 4));
+  var clientId = (typeof grunt.option('clientId') === 'undefined') ? env.clientId : grunt.option('clientId');
+  var clientSecret = (typeof grunt.option('clientSecret') === 'undefined') ? env.clientSecret : grunt.option('clientSecret');
+  var originUri = (typeof grunt.option('originUri') === 'undefined') ? env.originUri : grunt.option('originUri');
+  var uiHost = (typeof grunt.option('uiHost') === 'undefined') ? env.uiHost : grunt.option('uiHost');
+  var serviceUrl = (typeof grunt.option('serviceUrl') === 'undefined') ? env.serviceUrl : grunt.option('serviceUrl');
+  var solutionInstancesUrl = (typeof grunt.option('solutionInstancesUrl') === 'undefined') ? env.solutionInstancesUrl : grunt.option('solutionInstancesUrl');
+
+  var config = {
+    buildDir : './build',
+    buildSdkDir : './build-sdk',
+    artifactDir : './build-artifacts',
+    concatAppDir: './assets/concat/js/app.js',
+    concatCssDir: './assets/concat/css/main.css',
+    modules: './app/modules',
+    scss: './app/scss',
+    target: target,
+    version: pkgJson.version,
+    name: appName,
+    userHomeDir: process.env['HOME'],
+    clientId: clientId,
+    clientSecret: clientSecret,
+    originUri: originUri,
+    uiHost: uiHost,
+    buildArtifact: buildArtifact,
+    serviceUrl: serviceUrl,
+    solutionInstancesUrl: solutionInstancesUrl
+  };
+
+  // now build the appConfig-env.json
+  var appConfigEnv = {
+    originUri: config.originUri,
+    serviceUrl: config.serviceUrl,
+    solutionInstancesUrl: config.solutionInstancesUrl
+  };
+
+  grunt.file.write('appConfig-env.json', JSON.stringify(appConfigEnv, null, 4));
+
+  // now build the appConfig-build.json
+  var appConfigBuild = {
+    app: appName,
+    version: config.version,
+    target: config.target,
+    buildDate: grunt.template.today('yyyymmddhhMMss'),
+    buildArtifact: config.buildArtifact
+  };
+
+  grunt.file.write('appConfig-build.json', JSON.stringify(appConfigBuild, null, 4));
 
   var tasks = ['watch','sass','browserSync','postcss','clean','compress','copy','filerev','useminPrepare',
   'useminPreparesdk','usemin','useminsdk','uglify','jshint','ngtemplates','processhtml','babel','ngAnnotate',
