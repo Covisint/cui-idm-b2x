1c952c35d507a115451b81dc22b56b6a3f5195fc
Piotr Bogun <piotr.bogun@thirdwavellc.com>
2016-10-25 12:23:00 -0500

    Fixes copy task to work with i18n versioned directory

--- tasks/copy.js
+++ tasks/copy.js
@@ -1,11 +1,15 @@
-module.exports = function(grunt,config){
+module.exports = function(grunt, config) {
   return {
     build: {
       files: [
         {
           src: 'index.html',
           dest: '<%= config.buildDir %>/index.html'
-        }, 
+        },
+        {
+          src: 'package.json',
+          dest: '<%= config.buildDir %>/package.json'
+        },
         {
           expand: true,
           cwd: 'unsupported/',
@@ -14,12 +18,12 @@ module.exports = function(grunt,config){
         },
         {
           src: [
-            'node_modules/@covisint/cui-i18n/dist/cui-i18n/angular-translate/*.json',
+            'node_modules/@covisint/cui-i18n/dist/<%= config.i18nVersion %>/cui-i18n/angular-translate/*.json',
             'node_modules/angular-i18n/*.js',
             'node_modules/@covisint/cui-icons/iconList',
             'node_modules/@covisint/cui-icons/dist/**/*.svg',
-            'node_modules/@covisint/cui-i18n/dist/cui-i18n/angular-translate/countries/*.json',
-            'node_modules/@covisint/cui-i18n/dist/cui-i18n/angular-translate/timezones/*.json',
+            'node_modules/@covisint/cui-i18n/dist/<%= config.i18nVersion %>/cui-i18n/angular-translate/countries/*.json',
+            'node_modules/@covisint/cui-i18n/dist/<%= config.i18nVersion %>/cui-i18n/angular-translate/timezones/*.json',
             'node_modules/lato-font/fonts/lato-normal/**',
             'node_modules/lato-font/fonts/lato-bold/**',
             'node_modules/lato-font/fonts/lato-black/**',
