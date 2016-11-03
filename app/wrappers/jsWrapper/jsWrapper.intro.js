c2c90a51ac75e04844a62bb5d2d82981355bfe66
Piotr Bogun <piotr.bogun@thirdwavellc.com>
2016-11-03 11:04:52 -0500

    Removes previous commit b37ad9

--- app/wrappers/jsWrapper/jsWrapper.intro.js
+++ app/wrappers/jsWrapper/jsWrapper.intro.js
@@ -4,19 +4,19 @@
 	let packageJson
 	let i18nPackageJson
 
-	$.ajax({ url: './appConfig.json', cache: false })
+	$.get('./appConfig.json')
 	.then(function(configData) {
 		appConfig = Object.assign({}, appConfig, configData)
-		return $.ajax({ url: './appConfig-env.json', cache: false })
+		return $.get('./appConfig-env.json')
 	})
 	.then(function(envConfigData) {
 		appConfig = Object.assign({}, appConfig, envConfigData)
-		return $.ajax({ url: './package.json', cache: false })
+		return $.get('./package.json')
 	})
 	.then(function(packageJsonData) {
 		packageJson = Object.assign({}, packageJson, packageJsonData)
 		if (appConfig.languageResources.dependencyOrigin === 'cui-idm-b2x') {
-			return $.ajax({ url: './node_modules/@covisint/cui-i18n/package.json', cache: false })
+			return $.get('./node_modules/@covisint/cui-i18n/package.json')
 		}
 		else return undefined
 	})
@@ -24,3 +24,5 @@
 		if (i18nPackageJsonData !== undefined) {
 			i18nPackageJson = Object.assign({}, i18nPackageJson, i18nPackageJsonData)	
 		}
+	})
+	.always(function() {
