fe18823585727b03c855fbe7cbad6aa3ee24bf35
Piotr Bogun <piotr.bogun@thirdwavellc.com>
2016-11-21 16:27:09 -0600

    Adds logic to get/set user roles. Adds documentation for future developers

--- app/modules/common/factories/User.factory.js
+++ app/modules/common/factories/User.factory.js
@@ -1,9 +1,17 @@
 angular.module('common')
-.factory('User',['$rootScope',($rootScope) => {
+.factory('User', ($rootScope) => {
+
+    /*
+        This factory is intended to store data/logic pertaining to the logged in user only.
+        This data is populated right after logon (this can be found in the API.factory.js 
+        inside the populateUserInfo method). The application will not run if those endpoints
+        fail.
+    */
 
     let user = {
-        entitlements: undefined
-    };
+        entitlements: undefined,
+        roles: undefined
+    }
 
     return {
         set : (newUser) => {
@@ -13,13 +21,18 @@ angular.module('common')
         get : () => user.cuid || '[cuid]',
 
         setEntitlements : (newEntitlements) => {
-            user.entitlements? user.entitlements = user.entitlements.concat(newEntitlements) : user.entitlements = newEntitlements;
+            user.entitlements ? user.entitlements = user.entitlements.concat(newEntitlements) : user.entitlements = newEntitlements
         },
 
         getEntitlements : () => user.entitlements,
 
-        user // get all the user info
+        setRoles : (newRoles) => {
+            user.roles ? user.roles = user.roles.concat(newRoles) : user.roles = newRoles
+        },
 
-    };
+        getRoles : () => user.roles,
 
-}]);
+        user
+    }
+
+})
