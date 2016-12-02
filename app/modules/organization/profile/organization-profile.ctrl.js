dbe8f2164bc7d7fddcd7d1bd5b97906a170b5375
Suchin Byreshappa <Suchin.Byreshappa@happiestminds.com>
2016-12-02 15:59:30 +0530

    Org profile page changes to get and display authentication policy name.

--- app/modules/organization/profile/organization-profile.ctrl.js
+++ app/modules/organization/profile/organization-profile.ctrl.js
@@ -11,13 +11,15 @@ angular.module('organization')
     if (storedData !== undefined) {
         orgProfile.securityAdmins = storedData.admins
         orgProfile.passwordPolicy = storedData.passwordPolicy
+        orgProfile.authenticationPolicy=storedData.authenticationPolicy
     }
     else Loader.onFor('orgProfile.init')
 
-    Organization.initOrganizationProfile(orgProfile.organization.id, orgProfile.organization.passwordPolicy.id)
+    Organization.initOrganizationProfile(orgProfile.organization.id, orgProfile.organization.passwordPolicy.id, orgProfile.organization.authenticationPolicy.id)
     .then(res => {
         orgProfile.securityAdmins = res.admins
         orgProfile.passwordPolicy = res.passwordPolicy
+        orgProfile.authenticationPolicy=res.authenticationPolicy
         DataStorage.setType('orgProfile', res)
         Loader.offFor('orgProfile.init')
     })
