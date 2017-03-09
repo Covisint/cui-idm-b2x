185200bbdd8d8473b893ac90809606604fb1e34a
Suchin Byreshappa <Suchin.Byreshappa@happiestminds.com>
2017-03-09 15:25:52 +0530

    changed state param id to match in all pages

--- app/modules/organization/organization.module.js
+++ app/modules/organization/organization.module.js
@@ -79,19 +79,19 @@ angular.module('organization', [])
             access: loginRequired
         })
         .state('organization.requests.newGrant', {
-            url: '/new-grant?userID',
+            url: '/new-grant?userId',
             templateUrl: templateBase + 'requests/newGrant/requests-newGrant.html',
             controller: returnCtrlAs('newGrant'),
             access: loginRequired
         })
         .state('organization.requests.newGrantSearch', {
-            url: '/search?type&category&name&userID&page&pageSize&sortBy',
+            url: '/search?type&category&name&userId&page&pageSize&sortBy',
             templateUrl: templateBase + 'requests/newGrant/search/search.html',
             controller: returnCtrlAs('newGrantSearch'),
             access: loginRequired
         })
         .state('organization.requests.newGrantClaims', {
-            url: '/claims?userID',
+            url: '/claims?userId',
             templateUrl: templateBase + 'requests/newGrant/claims/claims.html',
             controller: returnCtrlAs('newGrantClaims'),
             access: loginRequired
