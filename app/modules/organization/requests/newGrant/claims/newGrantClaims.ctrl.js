ea422278396c7861ff73bfd37541ee5feb8e555c
Suchin Byreshappa <Suchin.Byreshappa@happiestminds.com>
2017-03-14 12:28:40 +0530

    Success message and proper way to call claim grant call

--- app/modules/organization/requests/newGrant/claims/newGrantClaims.ctrl.js
+++ app/modules/organization/requests/newGrant/claims/newGrantClaims.ctrl.js
@@ -10,8 +10,8 @@ angular.module('organization')
 
     /* -------------------------------------------- ON LOAD START --------------------------------------------- */
 
-    NewGrant.pullFromStorage(newGrantClaims);
-    if (!newGrantClaims.appsBeingRequested) {
+    NewGrant.pullFromStorage(newGrantClaims,$stateParams.userId,'person');
+    if (newGrantClaims.numberOfRequests===0) {
         $state.go('organization.requests.newGrantSearch',{userId:$stateParams.userId})
     }
     // get the claims for each app being requested
@@ -64,19 +64,29 @@ angular.module('organization')
 
     newGrantClaims.submit = () => {
         Loader.onFor(loaderType + 'submit')
-
+        let claimsPromises=[]
         // Grant Packages
-        $q.all(NewGrant.packageGrants($stateParams.userId, newGrantClaims.packageRequests).map(opts => API.cui.grantPersonPackage(opts)))
+        $q.all(NewGrant.packageGrants($stateParams.userId ,'person', newGrantClaims.packageRequests).map(opts => API.cui.grantPersonPackage(opts)))
         .then(res => {
             // grant claims
-            return $q.all(NewGrant.claimGrants($stateParams.userId, newGrantClaims.packageRequests).map(opts => API.cui.grantClaims(opts)))
+            let claimsData=NewGrant.claimGrants($stateParams.userId ,'person', newGrantClaims.packageRequests)
+            claimsData.forEach(claimData => {
+                if(claimData.data.packageClaims&&claimData.data.packageClaims.length!==0){
+                    claimsPromises.push(API.cui.grantClaims(claimData))
+                }
+            })
+            return $q.all(claimsPromises)
         })
         .then(res => {
             Loader.offFor(loaderType + 'submit')
             newGrantClaims.success = true
+            DataStorage.setType('newGrant',{})
+            $timeout(() => {
+                $state.go('organization.directory.userList');
+            }, 3000);
         })
         .catch(err => {
-            Loader.onFor(loaderType + 'submit')
+            Loader.offFor(loaderType + 'submit')
             newGrantClaims.error = true
         })
     }
