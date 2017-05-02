9560dd560e04cc8b72bde8c13eb5c87f81ca2d92
Sathishkumar Mahalingam <Sathishkumar.m@happiestminds.com>
2017-05-02 18:15:45 +0530

    Person suspend, unsuspend and password reset functionality for admin code changes added.

--- app/modules/organization/directory/user-details/directory-userDetails.ctrl.js
+++ app/modules/organization/directory/user-details/directory-userDetails.ctrl.js
@@ -1,5 +1,5 @@
 angular.module('organization')
-.controller('userDetailsCtrl', function(API, Loader, CuiMobileNavFactory, $scope, $stateParams) {
+.controller('userDetailsCtrl', function(API, Loader, CuiMobileNavFactory, $scope, $stateParams,APIError,APIHelpers,$timeout,$q) {
 
     const userDetails = this
     const scopeName = 'userDetails.'
@@ -11,7 +11,7 @@ angular.module('organization')
     /* -------------------------------------------- ON LOAD START --------------------------------------------- */
 
     Loader.onFor(scopeName + 'userInfo')
-
+/*
     API.cui.getPerson({ personId: $stateParams.userId })
     .then(res => {
         userDetails.user = res
@@ -23,8 +23,304 @@ angular.module('organization')
     .always(() => {
         Loader.offFor(scopeName + 'userInfo')
         $scope.$digest()
+    })*/
+
+
+    const apiPromises = [
+        API.cui.getPerson({
+            personId: $stateParams.userId
+        }),
+
+        API.cui.getOrganization({ organizationId: $stateParams.orgId  })
+    ]
+
+    $q.all(apiPromises)
+    .then(([ user, organization ]) => {
+        CuiMobileNavFactory.setTitle(user.name.given + '.' + user.name.surname)
+       userDetails.user = Object.assign({}, user)
+        userDetails.organization = Object.assign({}, organization);
+        return API.cui.getPasswordPolicy({ policyId: userDetails.organization.passwordPolicy.id })
+    })
+    .then(res => {
+        userDetails.passwordPolicy = res
+
+        res.rules.forEach(rule => {
+            if (rule.type === 'history') {
+                userDetails.numberOfPasswords = rule.numberOfPasswords
+            }
+        })
+        Loader.offFor(scopeName + 'userInfo')
+    })
+    .catch(() => {
+        Loader.offFor(scopeName + 'userInfo')
+        APIError.onFor('userDetails.user')
     })
 
     /* --------------------------------------------- ON LOAD END ---------------------------------------------- */
+
+    userDetails.suspend = (personId) => {
+
+        userDetails.suspend.begun = (userDetails.suspend.begun)? false:true
+        userDetails.unsuspend.begun = (userDetails.unsuspend.begun)? false:false
+        userDetails.specifyPassword.begun = (userDetails.specifyPassword.begun)? false:false
+
+        const name = 'userDetails.suspend'
+
+        userDetails.suspend.reset = () => {
+            Loader.offFor(name)
+            APIError.offFor(name)
+            userDetails.user.suspendReason = ''
+            userDetails.suspend.success && delete userDetails.suspend.success
+        }
+
+        userDetails.suspend.reset()
+
+        userDetails.suspend.confirm = () => {
+            Loader.onFor(name)
+            APIError.offFor(name)
+
+            const reason = encodeURIComponent(userDetails.user.suspendReason)
+
+            API.cui.suspendPerson({
+                qs: APIHelpers.getQs({
+                    personId,
+                    reason
+                })
+            })
+            .then(
+                res => {
+                    APIError.offFor(name)
+                    userDetails.suspend.success = true
+                    userDetails.user.status = 'suspended'
+                    $timeout(userDetails.suspend.cancel, 1500)
+                },
+                err => {
+                    APIError.onFor(name)
+                }
+            )
+            .always(() => {
+                Loader.offFor(name)
+                $scope.$digest()
+            })
+        }
+
+        userDetails.suspend.cancel = () => {
+            userDetails.suspend.begun = false
+            userDetails.suspend.reset()
+        }
+    }
+
+    userDetails.unsuspend = (personId) => {
+        userDetails.unsuspend.begun = (userDetails.unsuspend.begun)? false:true
+        userDetails.suspend.begun = (userDetails.suspend.begun)? false:false
+        userDetails.specifyPassword.begun = (userDetails.specifyPassword.begun)? false:false
+
+        const name = 'userDetails.unsuspend'
+
+        userDetails.unsuspend.reset = () => {
+            Loader.offFor(name)
+            APIError.offFor(name)
+            userDetails.user.unsuspendReason = ''
+            userDetails.unsuspend.success && delete userDetails.unsuspend.success
+        }
+
+        userDetails.unsuspend.reset()
+
+        userDetails.unsuspend.confirm = () => {
+            Loader.onFor(name)
+            APIError.offFor(name)
+
+            const reason = encodeURIComponent(userDetails.user.unsuspendReason)
+
+            API.cui.unsuspendPerson({
+                qs: APIHelpers.getQs({
+                    personId,
+                    reason
+                })
+            })
+            .then(
+                res => {
+                    APIError.offFor(name)
+                    userDetails.unsuspend.success = true
+                    userDetails.user.status = 'active'
+                    $timeout(userDetails.unsuspend.cancel, 1500)
+                },
+                err => {
+                    APIError.onFor(name)
+                }
+            )
+            .always(() => {
+                Loader.offFor(name)
+                $scope.$digest()
+            })
+        }
+
+        userDetails.unsuspend.cancel = () => {
+            userDetails.unsuspend.begun = false
+            userDetails.unsuspend.reset()
+        }
+    }
+
+    userDetails.specifyPassword = () => {
+        errorTimer && $timeout.cancel(errorTimer) // cancel the timer if there's one pending
+        let errorTimer
+
+        const name = 'userDetails.specifyPassword'
+        userDetails.specifyPassword.begun = (userDetails.specifyPassword.begun)? false:true
+        userDetails.suspend.begun = (userDetails.suspend.begun)? false:false
+        userDetails.unsuspend.begun = (userDetails.unsuspend.begun)? false:false
+
+        userDetails.specifyPassword.reset = () => {
+            Loader.offFor(name)
+            APIError.offFor(name)
+            APIError.offFor('userDetails.passwordHistory')
+            userDetails.specifyPassword.success && delete userDetails.specifyPassword.success
+            userDetails.specifyPassword.passwordHistoryError = false
+            userDetails.specifyPassword.newPassword = ''
+            userDetails.specifyPassword.newPasswordConfirm = ''
+        }
+
+        userDetails.specifyPassword.validate = (password, formObject, input) => {
+            const validSwitch = (input, isValidBoolean) => {
+                switch (input) {
+                    case 'newPassword':
+                        userDetails.specifyPassword.validNewPassword = isValidBoolean
+                    case 'newPasswordConfirm':
+                        userDetails.specifyPassword.validNewPasswordRe = isValidBoolean
+                }
+            }
+
+            const validateData = {
+                userId: $stateParams.userId,
+                organizationId: $stateParams.orgId,
+                password: password,
+                operations: ['PASSWORD_SPECIFY']
+            }
+
+            API.cui.validatePassword({data: validateData})
+            .then(res => {
+                let validPasswordHistory = false
+
+                res.forEach(rule => {
+                    if (rule.type === 'HISTORY' && rule.isPassed) {
+                        validPasswordHistory = true
+                        return
+                    }
+                })
+
+                if (validPasswordHistory) {
+                    validSwitch(input, true)
+                    formObject[input].$setValidity(input, true)
+                    $scope.$digest()
+                }
+                else {
+                    validSwitch(input, false)
+                    formObject[input].$setValidity(input, false)
+                    $scope.$digest()
+                }
+            })
+            .fail(err => {
+                validSwitch(input, false)
+                formObject[input].$setValidity(input, false)
+                $scope.$digest()
+            })
+        }
+
+        userDetails.specifyPassword.confirm = () => {
+            APIError.offFor(name)
+            Loader.onFor(name)
+
+          /*  if (!userDetails.specifyPassword.form.$valid) {
+                angular.forEach(userDetails.specifyPassword.form.$error, error => {
+                    angular.forEach(error, errorField => errorField.$setTouched())
+                })
+                APIError.onFor(name)
+                return
+            }*/
+
+       /*     const apiPromisesPwd = [
+                API.cui.getPersonPassword({personId: $stateParams.userId})
+                .then(res => {
+                    const version = res.version
+
+                     API.cui.resetPersonPassword({
+                        qs: [['subject', $stateParams.userId]],
+                        data: {
+                            version: version,
+                            subject: $stateParams.userId,
+                            password: userDetails.specifyPassword.newPassword,
+                            passwordPolicyId: userDetails.passwordPolicy.id,
+                            authenticationPolicyId: userDetails.organization.authenticationPolicy.id
+                        }
+                    })
+                })
+                .then(res => {
+                    const versions = res.version
+
+                     API.cui.expirePersonPassword({
+                        qs: [['subject', $stateParams.userId]],
+                        data: {
+                            version: versions,
+                            subject: $stateParams.userId,
+                            password: userDetails.specifyPassword.newPassword,
+                            passwordPolicyId: userDetails.passwordPolicy.id,
+                            authenticationPolicyId: userDetails.organization.authenticationPolicy.id
+                        }
+                    })
+                })
+            ]
+
+            $q.all(apiPromisesPwd)
+            .then(() => {
+                userDetails.specifyPassword.success = true
+                $timeout(userDetails.specifyPassword.cancel, 1500)
+            })
+            .catch(() => {
+                APIError.onFor(name)
+                errorTimer = $timeout(() => APIError.offFor(name), 1500)
+            })*/
+
+            API.cui.getPersonPassword({personId: $stateParams.userId})
+            .then(res => {
+                return API.cui.resetPersonPassword({
+                    qs: [['subject', $stateParams.userId]],
+                    data: {
+                        subject: $stateParams.userId,
+                        password: userDetails.specifyPassword.newPassword,
+                        passwordPolicyId: userDetails.passwordPolicy.id,
+                        authenticationPolicyId: userDetails.organization.authenticationPolicy.id
+                    }
+                })
+            })
+            .then(res => {
+                return API.cui.expirePersonPassword({
+                    qs: [['subject', $stateParams.userId]],
+                    data: {
+                        subject: $stateParams.userId,
+                        password: userDetails.specifyPassword.newPassword,
+                        passwordPolicyId: userDetails.passwordPolicy.id,
+                        authenticationPolicyId: userDetails.organization.authenticationPolicy.id
+                    }
+                })
+            })
+            .then(res => {
+                userDetails.specifyPassword.success = true
+                $timeout(userDetails.specifyPassword.cancel, 1500)
+            }, err => {
+                console.log(err)
+                APIError.onFor(name)
+                errorTimer = $timeout(() => APIError.offFor(name), 1500)
+            })
+            .always(() => {
+                Loader.offFor(name)
+                $scope.$digest()
+            })
+        }
+
+        userDetails.specifyPassword.cancel = () => {
+            userDetails.specifyPassword.begun = false
+            userDetails.specifyPassword.reset()
+        }
+    }
 
 })
