dd267350d92a1f238f24e071cac7767693fc25ce
Suchin Byreshappa <Suchin.Byreshappa@happiestminds.com>
2017-05-10 17:58:25 +0530

    Fixed count issue in popup

--- app/modules/applications/myApplications/myApplications.ctrl.js
+++ app/modules/applications/myApplications/myApplications.ctrl.js
@@ -104,7 +104,9 @@ angular.module('applications')
         $q.all(promises)
         .then(res => {
             myApplications.viewList = Object.assign(res[0]).filter(x => x.servicePackage.displayable===true&&x.grant.status=='active')
-            myApplications.count = res[1]
+            if (!previouslyLoaded) {
+                myApplications.count = res[1]
+            }
             myApplications.popupCount = myApplications.count-Object.assign(res[0]).filter(x => x.servicePackage.displayable!==true || x.grant.status!=='active').length
             myApplications.list=res[0];
             // re-render pagination if available
