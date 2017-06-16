cd07df774140b7572128b830da6cbc59ff0ff65f
Suchin Byreshappa <Suchin.Byreshappa@happiestminds.com>
2017-06-16 11:11:51 +0530

    Added proper Loading, Pagination to display users, Note: OrgCount is giving 403 for now, Commented out.

--- app/modules/misc/search/search.ctrl.js
+++ app/modules/misc/search/search.ctrl.js
@@ -1,53 +1,27 @@
 angular.module('misc')
-    .controller('searchCtrl', ['API', '$scope', '$stateParams', '$state', 'AppRequests', 'localStorageService', '$q', '$pagination', function(API, $scope, $stateParams, $state, AppRequests, localStorage, $q, $pagination) {
+    .controller('searchCtrl', ['API', '$scope', '$stateParams', '$state', 'AppRequests', 'localStorageService', '$q', '$pagination','APIHelpers','Loader', 
+    function(API, $scope, $stateParams, $state, AppRequests, localStorage, $q, $pagination,APIHelpers,Loader) {
         let search = this;
+        search.currentParentOrg = API.user.organization.id;
 
-        $scope.currentParentOrg = API.user.organization.id;
+        search.users = null;
+        search.timer = null
+        search.searchType = "organizations";
+        search.searchOrgId = "";
+        search.searchterms=""
+        search.count=1000
 
-        $scope.users = null;
-        $scope.timer = null
-        $scope.searchType = "people";
-        $scope.searchOrgId = "";
+        // search.keypress = function() {
+        //     search.users = [];
+        //     if (search.timer) { clearTimeout(search.timer); }
+        //     search.timer = window.setTimeout(function() {
+        //         search.searchNow();
+        //     }, 200);
+        // }
 
-        $scope.keypress = function() {
-            $scope.users = [];
-            if ($scope.timer) { clearTimeout($scope.timer); }
-            $scope.timer = window.setTimeout(function() {
-                $scope.searchNow();
-            }, 200);
-        }
+        /* -------------------------------------------- HELPER FUNCTIONS END --------------------------------------------- */
 
-        var orgDirectory = this
-        orgDirectory.search = {}
-        orgDirectory.sortBy = {}
-        orgDirectory.organizationList = null;
-
-        API.cui.getOrganizationHierarchy({ organizationId: $scope.currentParentOrg })
-            .done(personResponse => {
-                $scope.orgs = $scope.flattenOrgHierarchy(personResponse);
-            })
-            .fail(error => {
-            })
-
-        $scope.userClick = function(clickedUser) {
-
-            const stateOpts = {
-                userId: clickedUser.id,
-                orgId: clickedUser.organization.id,
-            }
-            if (clickedUser.status === 'pending') $state.go('organization.requests.personRequest', stateOpts)
-            else $state.go('organization.directory.userDetails', stateOpts)
-
-        }
-
-        $scope.orgClick = function(clickedOrd) {
-            const stateOpts = {
-                orgId: clickedOrd.id,
-            }
-            $state.go('organization.directory.orgDetails', stateOpts)
-        }
-
-        $scope.flattenOrgHierarchy = function(orgHierarchy) {
+        search.flattenOrgHierarchy = function(orgHierarchy) {
             /*
                 Takes the organization hierarchy response and returns a flat object array containing the id's and name's of
                 the top level organization as well as it's divisions.
@@ -86,84 +60,129 @@ angular.module('misc')
             }
         }
 
-        $scope.addPerson = function(newPerson) {
+        search.addPerson = function(newPerson) {
 
             // Prevent Duplicates
             var add = true;
-            $scope.users.forEach(function(person) {
+            search.users.forEach(function(person) {
                 if (newPerson.id == person.id) { add = false; }
             })
 
             if (newPerson.status == 'pending') { add = false; }
-            if (add) { $scope.users.push(newPerson) }
+            if (add) { search.users.push(newPerson) }
 
         }
 
-        $scope.searchNow = function() {
-
-            $scope.users = [];
-            $scope.orgs = [];
-
-            if ($scope.searchterms) {
-
-                if ($scope.searchType == "people") {
-                    
-                    API.cui.getPersons({
-                            qs: [
-                                ['fullName', $scope.searchterms]
-                            ]
-                        })
-                        .done(personResponse => {
-
-                            personResponse.forEach(function(x) {
-                                $scope.addPerson(x);
-                            })
-
-                            $scope.$apply();
-                        })
-                        .fail(error => {
-
-
-                        })
-                    API.cui.getPersons({
-                            qs: [
-                                ['email', $scope.searchterms]
-                            ]
-                        })
-                        .done(personResponse => {
-
-                            personResponse.forEach(function(x) {
-                                $scope.addPerson(x);
-                            })
-
-                            $scope.$apply();
-                        })
-                        .fail(error => {
-
-                        })
-
-                }
-
-                if ($scope.searchType == "organizations") {
-
-                    API.cui.getOrganizations({
-                            qs: [
-                                ['name', $scope.searchterms + "*"]
-                                // ['organization.id', 'OQ-ADM-PER-DEV01273701']
-                            ]
-                        })
-                        .done(orgsResponse => {
-
-                            $scope.orgs = orgsResponse;
-                            $scope.$apply();
-                        })
-                        .fail(error => {
-
-                        })
-
-
-                }
+        search.searchNow = function(type) {
+            if (type) {
+                search.searchParams.page=1
             }
+            Loader.onFor('search.loading')
+            search.users = [];
+            search.orgs = [];
+            let qsArray=APIHelpers.getQs(search.searchParams)
+            // if (search.searchterms|| !type) {
+
+                if (search.searchType == "people") {
+                    let qsArrayNameSearch=angular.copy(qsArray)
+                    qsArrayNameSearch.push(['fullName', search.searchterms])
+                    const promises= [API.cui.countPersons({qs:qsArrayNameSearch}),API.cui.getPersons({qs: qsArrayNameSearch})]
+                    $q.all(promises)
+                    .then(res=>{
+                        search.personCount=res[0]
+                        search.users=res[1]
+                        Loader.offFor('search.loading')
+                    })                    
+                    .catch(error => {
+                        Loader.offFor('search.loading')
+
+                    })
+                    //     qsArray.push(['email', search.searchterms])
+                    // API.cui.getPersons({
+                    //         qs: qsArray
+                    //     })
+                    //     .done(personResponse => {
+
+                    //         personResponse.forEach(function(x) {
+                    //             search.addPerson(x);
+                    //         })
+
+                    //         $scope.$digest()
+                    //     })
+                    //     .fail(error => {
+
+                    //     })
+
+                }
+                if (search.searchType == "organizations") {
+                    qsArray.push(['name', search.searchterms + "*"])
+                    API.cui.getOrganizations({qs: qsArray})
+                    .done(orgsResponse => {
+                        search.orgs = orgsResponse;
+                        Loader.offFor('search.loading')
+                        $scope.$digest()
+                    })
+                    .fail(error => {
+                        Loader.offFor('search.loading')
+                    })
+                }
+            // }
         }
 
+        /* -------------------------------------------- HELPER FUNCTIONS END --------------------------------------------- */
+
+        /* -------------------------------------------- ON LOAD START --------------------------------------------- */
+        search.searchParams = Object.assign({}, $stateParams)
+        search.searchParams.pageSize = search.searchParams.pageSize || $pagination.getUserValue() || $pagination.getPaginationOptions()[0]
+        if (!search.searchParams.page) {
+            search.searchParams.page=1
+        }
+        search.sortBy = {}
+        search.organizationList = null;
+        // API.cui.getOrganizationsCount({qs:APIHelpers.getQs(search.searchParams)})
+        // .then(count=>{
+        //     search.orgCount=count
+        //     return API.cui.getOrganizations({qs:APIHelpers.getQs(search.searchParams)})
+        // })
+        Loader.onFor('search.loading')
+        API.cui.getOrganizations({qs:APIHelpers.getQs(search.searchParams)})
+        .then(res => {
+            search.orgs = res
+            Loader.offFor('search.loading')
+            $scope.$digest()
+        })
+        .fail(error => {
+
+        })
+
+        /* -------------------------------------------- ON LOAD END --------------------------------------------- */
+
+        /* -------------------------------------------- ON CLICK FUNCTIONS START --------------------------------------------- */
+        search.updateSearchParams = function(page) {
+            if (page) {
+                search.searchParams.page=page
+            }
+            $state.transitionTo('search', search.searchParams, {notify: false})
+            search.searchNow()
+        }
+
+        search.userClick = function(clickedUser) {
+
+            const stateOpts = {
+                userId: clickedUser.id,
+                orgId: clickedUser.organization.id,
+            }
+            if (clickedUser.status === 'pending') $state.go('organization.requests.personRequest', stateOpts)
+            else $state.go('organization.directory.userDetails', stateOpts)
+
+        }
+
+        search.orgClick = function(clickedOrd) {
+            const stateOpts = {
+                orgId: clickedOrd.id,
+            }
+            $state.go('organization.directory.orgDetails', stateOpts)
+        }
+
+        /* -------------------------------------------- ON CLICK FUNCTIONS END --------------------------------------------- */
     }]);
