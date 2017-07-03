2d5bd82d4d8d3afe5d2d79d327a93cf0c8542fc0
Suchin Byreshappa <Suchin.Byreshappa@happiestminds.com>
2017-07-03 15:21:42 +0530

    Code Cleanup and more error handling

--- app/modules/common/factories/PersonAndOrgRequest.factory.js
+++ app/modules/common/factories/PersonAndOrgRequest.factory.js
@@ -1,5 +1,5 @@
 angular.module('common')
-.factory('PersonRequest', (API, APIError, CommonAPI, $q) => {
+.factory('PersonAndOrgRequest', (API, APIError, CommonAPI, $q) => {
 	'use strict'
 
 	/*
@@ -9,15 +9,15 @@ angular.module('common')
 		organization's data.
 	*/
 
-	const personRequestFactory = {}
-	const errorName = 'PersonRequestFactory.'
+	const PersonAndOrgRequestFactory = {}
+	const errorName = 'PersonAndOrgRequestFactory.'
 
 	/****************************************
 				Service Functions
 	****************************************/
 
 	// Returns the registration request associated with the userId
-	personRequestFactory.getPersonRegistrationRequest = (userId) => {
+	PersonAndOrgRequestFactory.getPersonRegistrationRequest = (userId) => {
 		const defer = $q.defer()
 
 		API.cui.getPersonRegistrationRequest({qs: [['registrant.id', userId]]})
@@ -33,26 +33,56 @@ angular.module('common')
 		return defer.promise
 	}
 
+	PersonAndOrgRequestFactory.getOrgRegistrationRequest = (organizationId) => {
+		const defer = $q.defer()
+
+		API.cui.getOrgRegistrationRequests({qs: [['organization.id', organizationId]]})
+		.done(registrationResponse => {
+			defer.resolve(registrationResponse[0])
+		})
+		.fail(err => {
+			console.error('Failed getting organization\'s registration request')
+			APIError.onFor(errorName + 'getRegistrationRequest')
+			defer.reject(err)
+		})
+
+		return defer.promise
+	}
+
 	/*
 		Wrapper call for: 	PersonRequest.getPersonRegistrationRequest()
 							CommonAPI.getPerson()
 							CommonAPI.getOrganization()
 	*/
-	personRequestFactory.getPersonRegistrationRequestData = (userId, organizationId) => {
+	PersonAndOrgRequestFactory.getRegistrationRequestData = (userId, organizationId, type) => {
 		const defer = $q.defer()
 		let callsCompleted = 0
 		let requestData = {}
 
-		personRequestFactory.getPersonRegistrationRequest(userId)
-		.then(registrationRequest => {
-			requestData.request = registrationRequest
-		})
-		.finally(() => {
-			callsCompleted += 1
-			if (callsCompleted === 3) {
-                defer.resolve(requestData);
-            }
-		})
+		if (type==='person') {
+			PersonAndOrgRequestFactory.getPersonRegistrationRequest(userId)
+			.then(registrationRequest => {
+				requestData.request = registrationRequest
+			})
+			.finally(() => {
+				callsCompleted += 1
+				if (callsCompleted === 3) {
+	                defer.resolve(requestData);
+	            }
+			})
+		}
+		else{
+			PersonAndOrgRequestFactory.getOrgRegistrationRequest(organizationId)
+			.then(registrationRequest => {
+				requestData.request = registrationRequest
+			})
+			.finally(() => {
+				callsCompleted += 1
+				if (callsCompleted === 3) {
+	                defer.resolve(requestData);
+	            }
+			})
+		}
 
 		CommonAPI.getPerson(userId)
 		.then(personData => {
@@ -81,7 +111,7 @@ angular.module('common')
 
 	// Provided a decision ('approved' or 'denied') and the person request object
 	// Handles the approval/denial of the person request
-	personRequestFactory.handleRequestApproval = (decision, request) => {
+	PersonAndOrgRequestFactory.handleRequestApproval = (decision, request) => {
 		let data = [['requestId', request.id]]
 
 		if (decision === 'approved') {
@@ -101,6 +131,6 @@ angular.module('common')
 		}
 	}
 
-	return personRequestFactory
+	return PersonAndOrgRequestFactory
 
 })
