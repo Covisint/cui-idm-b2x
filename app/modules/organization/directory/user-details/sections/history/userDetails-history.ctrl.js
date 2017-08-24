8bf9a07a5550fac779417b79bdddeebcf81cbfa2
Sathishkumar Mahalingam <Sathishkumar.m@happiestminds.com>
2017-08-24 14:23:08 +0530

    IDMUX-438 code changes.

--- app/modules/organization/directory/user-details/sections/history/userDetails-history.ctrl.js
+++ app/modules/organization/directory/user-details/sections/history/userDetails-history.ctrl.js
@@ -12,16 +12,28 @@ angular.module('organization')
     userDetailsHistory.sortClicked = false;
 
     // ON LOAD START ---------------------------------------------------------------------------------
-
-	apiPromises.push(
-		API.cui.getPersonStatusHistory({qs: [['userId', String(userId)]]})
-    	.then((res) => {
-    		userDetailsHistory.personStatusHistory = res;
-    	})
+    
+    let today=new Date()
+    let dd=today.getDate()
+    let yyyy=today.getFullYear()
+    let mmm=today.toString().substring(4,7);
+    let qsLastDate= dd+'-'+mmm+'-'+yyyy
+        
+	
+    apiPromises.push(
+		/*API.cui.getPersonStatusHistory({qs: [['userId', String(userId)]]}),*/
+        API.cui.getPersonDetailedStatusHistory({qs : [
+                ['userId', userId], 
+                ['startDate','01-Jan-2016'],
+                ['lastDate',qsLastDate]
+            ]})/*,
+         API.cui.getPasswordCangeHistory({personId:userId})*/
     );
 
     $q.all(apiPromises)
     .then((res) => {
+        userDetailsHistory.statusHistory = res[0]
+         /*userDetailsHistory.passwordChangeHistory = res[1]*/
     	userDetailsHistory.loading = false;
     })
     .catch((error) => {
