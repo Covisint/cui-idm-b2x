d46b23f54bd9f99433902a94e386f9b6dd5bddb1
Suchin Byreshappa <Suchin.Byreshappa@happiestminds.com>
2018-02-19 21:05:49 +0530

    New states for package edit and service edit

--- app/modules/administration/administration.module.js
+++ app/modules/administration/administration.module.js
@@ -14,10 +14,29 @@ angular.module('administration',[])
             access: loginRequired,
             abstract: true
         })
-        .state('administration.manageApplications', {
-            url: '/applications?page&pageSize&service.category',
+        // applications/package related
+        .state('administration.applications', {
+            url: '/applications',
+            template: '<div ui-view class="cui-administration"></div>',
+            access: loginRequired,
+            abstract: true
+        })
+        .state('administration.applications.manageApplications', {
+            url: '?page&pageSize&service.category',
             templateUrl: templateBase + 'applications/allApplications-manage.html',
             controller: returnCtrlAs('manageAllApplications'),
+            access:loginRequired
+        })
+        .state('administration.applications.editPackage', {
+            url: '/:pkgId/edit',
+            templateUrl: templateBase + 'applications/editPackage/editPackage.html',
+            controller: returnCtrlAs('editPackage'),
+            access:loginRequired
+        })
+        .state('administration.applications.editService', {
+            url: '/:serviceId/editService',
+            templateUrl: templateBase + 'applications/editService/editService.html',
+            controller: returnCtrlAs('editService'),
             access:loginRequired
         });
 }]);
